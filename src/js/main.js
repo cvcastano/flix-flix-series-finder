32e147c385c0ab04ede1e2714ff5ffb18551f122
cvcastano <carocastanocastillo@gmail.com>
2021-02-08 20:49:22 +0100

    handle input

--- src/js/main.js
+++ src/js/main.js
@@ -2,40 +2,59 @@
 
 const inputElement = document.querySelector('.js-filter');
 const showsContainer = document.querySelector('.js-shows');
+const buttonElement = document.querySelector('.js-search-button');
+const formElement = document.querySelector('.js-form');
 
 
 let shows = [];
 
-function fetchApiData() {
-    fetch(`http://api.tvmaze.com/search/shows?q=${'game'}`)
+function handleInput(ev) {
+    ev.preventDefault();
+   
+    console.log(inputElement.value);
+ fetchApiData();
+}
+formElement.addEventListener('submit', handleInput);    
+
+
+
+
+function fetchApiData() { 
+
+    fetch(`http://api.tvmaze.com/search/shows?q=${inputElement.value}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
             shows = data;
 
             renderShows();
+
         });
 }
-function renderShows () {
+function renderShows() {
     let htmlCode = '';
     for (let i = 0; i < shows.length; i++) {
         htmlCode += `<li class="js-show">`;
         htmlCode += `<h4 class="js-show-name">${shows[i].show.name}</h4>`
         htmlCode += `<img src="" alt="poster of the show ${shows[i].show.name}" class="js-show-img"/>`;
         htmlCode += `</li>`;
-    
+
     }
 
     showsContainer.innerHTML = htmlCode;
 }
-    
-function handleFilter () {
-    
 
-}
 
-inputElement.addEventListener('keyup', handleFilter)
 
-fetchApiData();
+
+
+
+
+
+
+
+// handleInput();
+
+
 
 
