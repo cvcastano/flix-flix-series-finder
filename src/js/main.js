37a4bf84dd3900aa4f9f24d686710ba030fcc0ec
cvcastano <carocastanocastillo@gmail.com>
2021-02-08 19:29:45 +0100

    render shows

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,41 @@
 'use strict';
 
-const showName = document.querySelector('.js-filter').value;
+const inputElement = document.querySelector('.js-filter');
+const showsContainer = document.querySelector('.js-shows');
+
 
 let shows = [];
-let images = [];
 
 function fetchApiData() {
     fetch(`http://api.tvmaze.com/search/shows?q=${'game'}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-       shows = data;
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            shows = data;
 
-      });
-  }
-  fetchApiData ();
+            renderShows();
+        });
+}
+function renderShows () {
+    let htmlCode = '';
+    for (let i = 0; i < shows.length; i++) {
+        htmlCode += `<li class="js-show">`;
+        htmlCode += `<h4 class="js-show-name">${shows[i].show.name}</h4>`
+        htmlCode += `<img src="" alt="poster of the show ${shows[i].show.name}" class="js-show-img"/>`;
+        htmlCode += `</li>`;
+    
+    }
+
+    showsContainer.innerHTML = htmlCode;
+}
+    
+function handleFilter () {
+    
+
+}
+
+inputElement.addEventListener('keyup', handleFilter)
+
+fetchApiData();
+
+
