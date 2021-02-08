40f99b0b7ca976cfb8b9dc930c95cca8d8174dfe
cvcastano <carocastanocastillo@gmail.com>
2021-02-08 18:09:08 +0100

    fetch API data

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('>> Ready :)');
+const showName = document.querySelector('.js-filter').value;
+
+let shows = [];
+let images = [];
+
+function fetchApiData() {
+    fetch(`http://api.tvmaze.com/search/shows?q=${'game'}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+       shows = data;
+
+      });
+  }
+  fetchApiData ();
